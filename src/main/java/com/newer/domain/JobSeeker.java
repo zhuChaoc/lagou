d7f44ac2c4ddac2298cde3d44722ae88e2949b32
zhanghu1996 <540935487@qq.com>
2019-04-02 16:12:46 +0800

    zbw第一次修改

---
+++ src/main/java/com/newer/domain/JobSeeker.java
@@ -0,0 +1,14 @@
+package com.newer.domain;
+
+import java.io.Serializable;
+
+public class JobSeeker implements Serializable {
+    private Integer JID;
+    public Integer getJID() {
+        return JID;
+    }
+
+    public void setJID(Integer JID) {
+        this.JID = JID;
+    }
+}
---
+++ src/test/com/newer/test/test1.java
@@ -0,0 +1,13 @@
+package com.newer.test;
+
+import com.newer.domain.JobSeeker;
+import org.junit.Test;
+
+public class test1 {
+    @Test
+    public void test(){
+        JobSeeker jobSeeker=new JobSeeker();
+        jobSeeker.setJID(111);
+        System.out.println(jobSeeker.getJID());
+    }
+}
