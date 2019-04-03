00f1d174d94cb813d9632dc4dc9b812ea11bc836
ningbo1018 <1227566261@qq.com>
2019-04-03 20:11:41 +0800

    user表，menu表和user_menu表的建表语句和数据。user表中增加了一个account字段

---
+++ src/main/resources/sql/menu.sql
@@ -0,0 +1,10 @@
+create table `menu` (
+	`id` int (11),
+	`name` varchar (60),
+	`linkurl` varchar (135)
+); 
+insert into `menu` (`id`, `name`, `linkurl`) values('1','首页','index.html');
+insert into `menu` (`id`, `name`, `linkurl`) values('2','公司','companylist.html');
+insert into `menu` (`id`, `name`, `linkurl`) values('3','论坛','forum.html');
+insert into `menu` (`id`, `name`, `linkurl`) values('4','简历管理','jianli.html');
+insert into `menu` (`id`, `name`, `linkurl`) values('5','发布职位','create.html');
