bff01c072fa912d500676511c2d3653132c81749
zhanghu1996 <540935487@qq.com>
2019-04-04 15:25:28 +0800

    新的实体类

---
+++ src/main/java/com/newer/domain/Company.java
@@ -0,0 +1,197 @@
+package com.newer.domain;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ *
+ * 公司基本信息类
+ */
+public class Company implements Serializable {
+    private Integer id;  //公司id
+    private String cpName; //公司名称
+    private String cpPhone;//公司联系方式
+    private String cpEmail;//公司邮箱
+    private String cpShort;//公司简称
+    private String cpLogo;//公司logo
+    private String cpLicense;//营业执照
+    private String cpUrl;//公司网址
+    private String cpCity;//所在城市
+    private String cpArea;//行业领域
+    private String cpSize;//公司规模
+    private String cpDevelop;//发展规模
+    private String cpInvestor;//投资机构
+    private String cpShortInfo;//公司简介
+    private CpInfo cpInfo;//公司背景
+    private List<CrTeam> crTeams;//创始团队
+    private List<CpProduct>cpProducts;//公司产品
+    private List<Job>jobs;//发布职位
+
+    public Company() {
+    }
+
+    public Company(Integer id, String cpName, String cpPhone, String cpEmail, String cpShort, String cpLogo, String cpLicense, String cpUrl, String cpCity, String cpArea, String cpSize, String cpDevelop, String cpInvestor, String cpShortInfo, CpInfo cpInfo, List<CrTeam> crTeams, List<CpProduct> cpProducts, List<Job> jobs) {
+        this.id = id;
+        this.cpName = cpName;
+        this.cpPhone = cpPhone;
+        this.cpEmail = cpEmail;
+        this.cpShort = cpShort;
+        this.cpLogo = cpLogo;
+        this.cpLicense = cpLicense;
+        this.cpUrl = cpUrl;
+        this.cpCity = cpCity;
+        this.cpArea = cpArea;
+        this.cpSize = cpSize;
+        this.cpDevelop = cpDevelop;
+        this.cpInvestor = cpInvestor;
+        this.cpShortInfo = cpShortInfo;
+        this.cpInfo = cpInfo;
+        this.crTeams = crTeams;
+        this.cpProducts = cpProducts;
+        this.jobs = jobs;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCpName() {
+        return cpName;
+    }
+
+    public void setCpName(String cpName) {
+        this.cpName = cpName;
+    }
+
+    public String getCpPhone() {
+        return cpPhone;
+    }
+
+    public void setCpPhone(String cpPhone) {
+        this.cpPhone = cpPhone;
+    }
+
+    public String getCpEmail() {
+        return cpEmail;
+    }
+
+    public void setCpEmail(String cpEmail) {
+        this.cpEmail = cpEmail;
+    }
+
+    public String getCpShort() {
+        return cpShort;
+    }
+
+    public void setCpShort(String cpShort) {
+        this.cpShort = cpShort;
+    }
+
+    public String getCpLogo() {
+        return cpLogo;
+    }
+
+    public void setCpLogo(String cpLogo) {
+        this.cpLogo = cpLogo;
+    }
+
+    public String getCpLicense() {
+        return cpLicense;
+    }
+
+    public void setCpLicense(String cpLicense) {
+        this.cpLicense = cpLicense;
+    }
+
+    public String getCpUrl() {
+        return cpUrl;
+    }
+
+    public void setCpUrl(String cpUrl) {
+        this.cpUrl = cpUrl;
+    }
+
+    public String getCpCity() {
+        return cpCity;
+    }
+
+    public void setCpCity(String cpCity) {
+        this.cpCity = cpCity;
+    }
+
+    public String getCpArea() {
+        return cpArea;
+    }
+
+    public void setCpArea(String cpArea) {
+        this.cpArea = cpArea;
+    }
+
+    public String getCpSize() {
+        return cpSize;
+    }
+
+    public void setCpSize(String cpSize) {
+        this.cpSize = cpSize;
+    }
+
+    public String getCpDevelop() {
+        return cpDevelop;
+    }
+
+    public void setCpDevelop(String cpDevelop) {
+        this.cpDevelop = cpDevelop;
+    }
+
+    public String getCpInvestor() {
+        return cpInvestor;
+    }
+
+    public void setCpInvestor(String cpInvestor) {
+        this.cpInvestor = cpInvestor;
+    }
+
+    public String getCpShortInfo() {
+        return cpShortInfo;
+    }
+
+    public void setCpShortInfo(String cpShortInfo) {
+        this.cpShortInfo = cpShortInfo;
+    }
+
+    public CpInfo getCpInfo() {
+        return cpInfo;
+    }
+
+    public void setCpInfo(CpInfo cpInfo) {
+        this.cpInfo = cpInfo;
+    }
+
+    public List<CrTeam> getCrTeams() {
+        return crTeams;
+    }
+
+    public void setCrTeams(List<CrTeam> crTeams) {
+        this.crTeams = crTeams;
+    }
+
+    public List<CpProduct> getCpProducts() {
+        return cpProducts;
+    }
+
+    public void setCpProducts(List<CpProduct> cpProducts) {
+        this.cpProducts = cpProducts;
+    }
+
+    public List<Job> getJobs() {
+        return jobs;
+    }
+
+    public void setJobs(List<Job> jobs) {
+        this.jobs = jobs;
+    }
+}
---
+++ src/main/java/com/newer/domain/Job.java
@@ -0,0 +1,173 @@
+package com.newer.domain;
+
+import java.io.Serializable;
+import java.sql.Date;
+
+public class Job implements Serializable {
+    private Integer id;//职位id
+    private String jobType;//职位类型
+    private String jobName;//职位名
+    private String dName;//部门名
+    private String jobQuality;//工作性质
+    private Integer minSal;//最低工资
+    private Integer maxSal;//最高工资
+    private String jobCity;//工作城市
+    private String jobExperience;//工作经验
+    private String jobEducation;//学历要求
+    private String jobLightPoint;//职位亮点
+    private String jobInfo;//职位描述
+    private String jobAddress;//工作地址
+    private Date publishTime;//发布时间
+    private String state;//职位状态
+    private Integer subTimes;//订阅次数
+
+    public Job() {
+    }
+
+    public Job(Integer id, String jobType, String jobName, String dName, String jobQuality, Integer minSal, Integer maxSal, String jobCity, String jobExperience, String jobEducation, String jobLightPoint, String jobInfo, String jobAddress, Date publishTime, String state, Integer subTimes) {
+        this.id = id;
+        this.jobType = jobType;
+        this.jobName = jobName;
+        this.dName = dName;
+        this.jobQuality = jobQuality;
+        this.minSal = minSal;
+        this.maxSal = maxSal;
+        this.jobCity = jobCity;
+        this.jobExperience = jobExperience;
+        this.jobEducation = jobEducation;
+        this.jobLightPoint = jobLightPoint;
+        this.jobInfo = jobInfo;
+        this.jobAddress = jobAddress;
+        this.publishTime = publishTime;
+        this.state = state;
+        this.subTimes = subTimes;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getJobType() {
+        return jobType;
+    }
+
+    public void setJobType(String jobType) {
+        this.jobType = jobType;
+    }
+
+    public String getJobName() {
+        return jobName;
+    }
+
+    public void setJobName(String jobName) {
+        this.jobName = jobName;
+    }
+
+    public String getdName() {
+        return dName;
+    }
+
+    public void setdName(String dName) {
+        this.dName = dName;
+    }
+
+    public String getJobQuality() {
+        return jobQuality;
+    }
+
+    public void setJobQuality(String jobQuality) {
+        this.jobQuality = jobQuality;
+    }
+
+    public Integer getMinSal() {
+        return minSal;
+    }
+
+    public void setMinSal(Integer minSal) {
+        this.minSal = minSal;
+    }
+
+    public Integer getMaxSal() {
+        return maxSal;
+    }
+
+    public void setMaxSal(Integer maxSal) {
+        this.maxSal = maxSal;
+    }
+
+    public String getJobCity() {
+        return jobCity;
+    }
+
+    public void setJobCity(String jobCity) {
+        this.jobCity = jobCity;
+    }
+
+    public String getJobExperience() {
+        return jobExperience;
+    }
+
+    public void setJobExperience(String jobExperience) {
+        this.jobExperience = jobExperience;
+    }
+
+    public String getJobEducation() {
+        return jobEducation;
+    }
+
+    public void setJobEducation(String jobEducation) {
+        this.jobEducation = jobEducation;
+    }
+
+    public String getJobLightPoint() {
+        return jobLightPoint;
+    }
+
+    public void setJobLightPoint(String jobLightPoint) {
+        this.jobLightPoint = jobLightPoint;
+    }
+
+    public String getJobInfo() {
+        return jobInfo;
+    }
+
+    public void setJobInfo(String jobInfo) {
+        this.jobInfo = jobInfo;
+    }
+
+    public String getJobAddress() {
+        return jobAddress;
+    }
+
+    public void setJobAddress(String jobAddress) {
+        this.jobAddress = jobAddress;
+    }
+
+    public Date getPublishTime() {
+        return publishTime;
+    }
+
+    public void setPublishTime(Date publishTime) {
+        this.publishTime = publishTime;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public Integer getSubTimes() {
+        return subTimes;
+    }
+
+    public void setSubTimes(Integer subTimes) {
+        this.subTimes = subTimes;
+    }
+}
