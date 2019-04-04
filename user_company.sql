f3de34bd3349b5c23dbe6aa765eacb1bc5aa01f9
zhuChaoc <1223638017@qq.com>
2019-04-04 14:05:44 +0800

    zcc
    公司表

---
+++ user_company.sql
@@ -0,0 +1,18 @@
+CREATE TABLE `lagou`.`user_company` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `userid` INT NOT NULL,
+  `cpid` INT NOT NULL,
+  PRIMARY KEY (`id`));
+
+  
+ insert into lagou.user_company values(null,1,1);
+insert into lagou.user_company values(null,2,2);
+insert into lagou.user_company values(null,3,3);
+insert into lagou.user_company values(null,4,4);
+insert into lagou.user_company values(null,5,5);
+insert into lagou.user_company values(null,6,6);
+insert into lagou.user_company values(null,7,7);
+insert into lagou.user_company values(null,8,8);
+insert into lagou.user_company values(null,9,9);
+insert into lagou.user_company values(null,10,10);
+SELECT * FROM lagou.user_company; 
