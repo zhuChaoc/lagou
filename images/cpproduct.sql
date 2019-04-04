750dea4fc794bdabc0e04bd5040089252d20a06b
zhuChaoc <1223638017@qq.com>
2019-04-04 13:56:42 +0800

    zcc
    公司表

---
+++ images/cpproduct.sql
@@ -0,0 +1,22 @@
+﻿create table lagou.cpproduct
+(
+  id           int auto_increment
+    primary key,
+  cpid			int							not null,  
+  Productname  varchar(20)                  not null,
+  Producturl   varchar(50)                  not null,
+  Productinfo  varchar(500)                 not null,
+  productphoto varchar(50) collate utf8_bin not null
+);
+INSERT INTO lagou.cpproduct (id, cpid,Productname, Producturl, Productinfo, productphoto) VALUES (1, 1,'创赢智能', 'https://www.duba.com', '选择这种产品一定会好', '1.jpg');
+INSERT INTO lagou.cpproduct (id,cpid, Productname, Producturl, Productinfo, productphoto) VALUES (2,2, '新达共创', 'https://www.duba.com', '选择这种产品一定会好', '2.jpg');
+INSERT INTO lagou.cpproduct (id, cpid,Productname, Producturl, Productinfo, productphoto) VALUES (3, 3,'芭缇雅', 'https://www.duba.com', '选择这种产品一定会好', '3.jpg');
+INSERT INTO lagou.cpproduct (id,cpid, Productname, Producturl, Productinfo, productphoto) VALUES (4, 4,'天魅', 'https://www.duba.com', '选择这种产品一定会好', '4.jpg');
+INSERT INTO lagou.cpproduct (id,cpid, Productname, Producturl, Productinfo, productphoto) VALUES (5, 5,'黛萌', 'https://www.duba.com', '选择这种产品一定会好', '5.jpg');
+INSERT INTO lagou.cpproduct (id,cpid, Productname, Producturl, Productinfo, productphoto) VALUES (6, 6,'可怩卡莱', 'https://www.duba.com', '选择这种产品一定会好', '6.jpg');
+INSERT INTO lagou.cpproduct (id,cpid, Productname, Producturl, Productinfo, productphoto) VALUES (7, 7,'贝诗', 'https://www.duba.com', '选择这种产品一定会好', '7.jpg');
+INSERT INTO lagou.cpproduct (id,cpid, Productname, Producturl, Productinfo, productphoto) VALUES (8, 8,'光迅', 'https://www.duba.com', '选择这种产品一定会好', '8.jpg');
+INSERT INTO lagou.cpproduct (id,cpid,Productname, Producturl, Productinfo, productphoto) VALUES (9, 9,'生益', 'https://www.duba.com', '选择这种产品一定会好', '9.jpg');
+INSERT INTO lagou.cpproduct (id,cpid,Productname, Producturl, Productinfo, productphoto) VALUES (10, 10,'斯维尔', 'https://www.duba.com', '选择这种产品一定会好', '10.jpg');
+
+select * from lagou.cpproduct;
---
+++ images/crteam.sql
@@ -0,0 +1,20 @@
+﻿create table crteam
+(
+  id         int auto_increment
+    primary key,
+  Createname varchar(20)  not null,
+  Job        varchar(20)  not null,
+  ownurl     varchar(45)  not null,
+  Owninfo    varchar(500) not null,
+  ownphoto   varchar(50)  not null
+);
+INSERT INTO employee.crteam (id, Createname, Job, ownurl, Owninfo, ownphoto) VALUES (1, '朱超城', 'CEO', 'github.com/zhuChaoc', '为人朴实正直，有积极进取的心态，努力拼搏。性格沉稳内敛，做事细心踏实，个性坚韧，能吃苦耐劳，对工作有很强的责任感！有很强进取心和团队协作精神，学习能力、适应能力、承受压力能力较强，善于挑战自我。', 'wyz.jpg');
+INSERT INTO employee.crteam (id, Createname, Job, ownurl, Owninfo, ownphoto) VALUES (2, '杨梦熊', 'CFO', 'github.com/zhuChaoc', '为人朴实正直，有积极进取的心态，努力拼搏。性格沉稳内敛，做事细心踏实，个性坚韧，能吃苦耐劳，对工作有很强的责任感！有很强进取心和团队协作精神，学习能力、适应能力、承受压力能力较强，善于挑战自我。', 'jcw.jpg');
+INSERT INTO employee.crteam (id, Createname, Job, ownurl, Owninfo, ownphoto) VALUES (3, '陈世', 'CEO', 'github.com/zhuChaoc', '为人朴实正直，有积极进取的心态，努力拼搏。性格沉稳内敛，做事细心踏实，个性坚韧，能吃苦耐劳，对工作有很强的责任感！有很强进取心和团队协作精神，学习能力、适应能力、承受压力能力较强，善于挑战自我。', 'pyy.jpg');
+INSERT INTO employee.crteam (id, Createname, Job, ownurl, Owninfo, ownphoto) VALUES (4, '晨风', '董事长', 'github.com/zhuChaoc', '为人朴实正直，有积极进取的心态，努力拼搏。性格沉稳内敛，做事细心踏实，个性坚韧，能吃苦耐劳，对工作有很强的责任感！有很强进取心和团队协作精神，学习能力、适应能力、承受压力能力较强，善于挑战自我。', 'gtl.jpg');
+INSERT INTO employee.crteam (id, Createname, Job, ownurl, Owninfo, ownphoto) VALUES (5, '唐浩', '董事长', 'github.com/zhuChaoc', '为人朴实正直，有积极进取的心态，努力拼搏。性格沉稳内敛，做事细心踏实，个性坚韧，能吃苦耐劳，对工作有很强的责任感！有很强进取心和团队协作精神，学习能力、适应能力、承受压力能力较强，善于挑战自我。', 'Jason.jpg');
+INSERT INTO employee.crteam (id, Createname, Job, ownurl, Owninfo, ownphoto) VALUES (6, '阿诺德', 'CFO', 'github.com/zhuChaoc', '为人朴实正直，有积极进取的心态，努力拼搏。性格沉稳内敛，做事细心踏实，个性坚韧，能吃苦耐劳，对工作有很强的责任感！有很强进取心和团队协作精神，学习能力、适应能力、承受压力能力较强，善于挑战自我。', 'Arnold.jpg');
+INSERT INTO employee.crteam (id, Createname, Job, ownurl, Owninfo, ownphoto) VALUES (7, '强森', 'CEO', 'github.com/zhuChaoc', '为人朴实正直，有积极进取的心态，努力拼搏。性格沉稳内敛，做事细心踏实，个性坚韧，能吃苦耐劳，对工作有很强的责任感！有很强进取心和团队协作精神，学习能力、适应能力、承受压力能力较强，善于挑战自我。', 'Dwayne.jpg');
+INSERT INTO employee.crteam (id, Createname, Job, ownurl, Owninfo, ownphoto) VALUES (8, '唐尼', '董事长', 'github.com/zhuChaoc', '为人朴实正直，有积极进取的心态，努力拼搏。性格沉稳内敛，做事细心踏实，个性坚韧，能吃苦耐劳，对工作有很强的责任感！有很强进取心和团队协作精神，学习能力、适应能力、承受压力能力较强，善于挑战自我。', 'Jr.jpg');
+INSERT INTO employee.crteam (id, Createname, Job, ownurl, Owninfo, ownphoto) VALUES (9, '纪宁', '董事长', 'github.com/zhuChaoc', '为人朴实正直，有积极进取的心态，努力拼搏。性格沉稳内敛，做事细心踏实，个性坚韧，能吃苦耐劳，对工作有很强的责任感！有很强进取心和团队协作精神，学习能力、适应能力、承受压力能力较强，善于挑战自我。', 'jl.jpg');
+INSERT INTO employee.crteam (id, Createname, Job, ownurl, Owninfo, ownphoto) VALUES (10, '石昊', 'CFO', 'github.com/zhuChaoc', '为人朴实正直，有积极进取的心态，努力拼搏。性格沉稳内敛，做事细心踏实，个性坚韧，能吃苦耐劳，对工作有很强的责任感！有很强进取心和团队协作精神，学习能力、适应能力、承受压力能力较强，善于挑战自我。', 'sh.jpg');
